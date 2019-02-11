c92da5d4843411cd8df79e53f15cbe9a0f19d502
Rodrigo Ziviani de Souza <rodrigo.ziviani@gmail.com>
2019-02-11 08:07:48 -0200

    First commit. Closes #1

---
+++ src/main/java/com/rodrigoziviani/rankcomplete/RankcompleteApplication.java
@@ -0,0 +1,14 @@
+package com.rodrigoziviani.rankcomplete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RankcompleteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RankcompleteApplication.class, args);
+	}
+
+}
+
